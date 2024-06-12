0c4a2d13a4e46d592a1f48309e337faae073d676
Jan Rębacz <janrebacz@vp.pl>
2024-06-12 16:06:24 +0200

    Update main.cpp
    Ścieżka do windows

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 int main() {
     Game game(800, 600);
+<<<<<<< Updated upstream
+=======
+    game.selectLevel("D:/gitklony/consoleOgienWoda/assets/level3.txt");
+>>>>>>> Stashed changes
     game.run();
     return 0;
 }
